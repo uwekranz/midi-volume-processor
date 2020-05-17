9959b801e778d67a82a3523f78b428ce8e517a9e
Uwe Kranz <ukranz@andrena.de>
2020-05-17 10:57:35 +0200

    add slider to gui

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -18,6 +18,17 @@ BasicPluginAudioProcessorEditor::BasicPluginAudioProcessorEditor (BasicPluginAud
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
     setSize (200, 200);
+
+    // these define the parameters of our slider object
+    midiVolume.setSliderStyle(Slider::LinearBarVertical);
+    midiVolume.setRange(0.0, 127.0, 1.0);
+    midiVolume.setTextBoxStyle(Slider::NoTextBox, false, 90, 0);
+    midiVolume.setPopupDisplayEnabled(true, false, this);
+    midiVolume.setTextValueSuffix(" Volume");
+    midiVolume.setValue(1.0);
+
+    // this function adds the slider to the editor
+    addAndMakeVisible(&midiVolume);
 }
 
 BasicPluginAudioProcessorEditor::~BasicPluginAudioProcessorEditor()
@@ -39,4 +50,7 @@ void BasicPluginAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+
+    // sets the position and size of the slider with arguments (x, y, width, height)
+    midiVolume.setBounds(40, 30, 20, getHeight() - 60);
 }
